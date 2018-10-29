39110b42a27792fe39215e718ee56a239ea46b9d
Paul Laffitte <paul.laffitte@epitech.eu>
2018-10-29 22:31:57 +0100

    alternative parser

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,13 @@ main = do
         Left a' -> a'
         Right a' -> a' : ('-' : b)
 
-    let (a2, b2) = runParser pBreak "break;"
+    let (a2, b2) = runParser pBreak ";break"
     print $ case a2 of
         Left a2' -> a2'
         Right a2' -> a2' ++ " - " ++ b2
+
+    -- let (a3, b3) = runParser pNumber "142"
+    -- print $ case a3 of
+    --     Left a3' -> a3'
+    --     Right a3' -> (show a3') ++ " - " ++ b3
+        
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,15 +1,22 @@
 module Parser where
 
+import Data.Char
+import Control.Applicative
+
 newtype Parser a = Parser
   { runParser :: String -> (Either String a, String)
   }
 
 instance Functor Parser where
-  fmap = undefined
+  fmap = fmapParser
 
 instance Applicative Parser where
   pure = returnParser
   (<*>) = sequentialParser
+
+instance Alternative Parser where
+  empty = emptyParser
+  (<|>) = pipeParser
 
 instance Monad Parser where
   return = returnParser
@@ -18,7 +25,19 @@ instance Monad Parser where
 -- fmapParser :: undefined
 
 sequentialParser :: Parser (a -> b) -> Parser a -> Parser b
-sequentialParser = undefined
+sequentialParser f x = do
+  f' <- f
+  x' <- x
+  return $ f' x'
+
+emptyParser :: Parser a
+emptyParser = Parser $ \x -> (Left "", x)
+
+pipeParser :: Parser a -> Parser a -> Parser a
+pipeParser x x' = Parser $ \str -> let (e, str') = runParser x str
+  in case e of
+    Left msg -> runParser x' str
+    Right v -> (Right v, str')
 
 returnParser :: a -> Parser a
 returnParser x = Parser $ \str -> (Right x, str)
@@ -42,6 +61,18 @@ pStatisfy predicat = Parser func
 pChar :: Char -> Parser Char
 pChar c = pStatisfy (== c)
 
+pMany :: Parser a -> Parser [a]
+pMany x = many_x
+  where
+    many_x = some_x <|> pure []
+    some_x = (:) <$> x <*> many_x
+
+pCharOf :: String -> Parser Char
+pCharOf cs = pStatisfy $ \c -> elem c cs
+
+pDigit :: Parser Integer
+pDigit = toInteger . digitToInt <$> pCharOf ['0'..'9']
+
 pString :: String -> Parser String
 pString [] = return []
 pString (x:xs) = do
@@ -49,5 +80,32 @@ pString (x:xs) = do
   xs' <- pString xs
   return (x':xs')
 
+pNumber :: Parser Integer
+pNumber = do
+  x <- pMany pDigit
+  return $ foldr (\x y -> y * 10 + x) 0 x
+
+-- pNumber :: Parser Integer
+-- pNumber = do
+--     x <- pDigit
+--     xs <- pNumber
+--     return $ (toInteger $ digitToInt x) * 10 + xs
+
 pBreak :: Parser String
-pBreak = pString "break"
+pBreak = (pString "break") <|> (pString ";")
+
+-- let number      = exp('number', /[0-9]*/);
+-- let strongOp    = exp('strongOp', /[*|/]/);
+-- let weakOp      = exp('weakOp', /\+|-/);
+
+-- let popen       = exp('popen', /\(/);
+-- let pclose      = exp('pclose', /\)/);
+
+-- let op          = exp('op', or(strongOp, weakOp));
+-- let value       = exp('value', and(maybe(weakOp), number));
+
+-- let func        = exp('func');
+-- let pexpr       = exp('pexpr');
+-- let expr        = exp('expr', and(or(value, pexpr), variadic(func)));
+-- func.value      = and(op, expr);
+-- pexpr.value     = and(maybe(weakOp), popen, expr, pclose);
