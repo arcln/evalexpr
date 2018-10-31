67c70f991eb3f5582f089b7465ccad5d5fed2ead
Arthur Chaloin <arthur@achaloin.com>
2018-10-31 13:37:06 +0100

    remove useless output

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,10 @@ import Eval
 import System.Environment
 
 start :: [String] -> IO ()
-start args = print $ eval $ head args
+start args = do
+  case eval $ head args of
+    Left err -> print err
+    Right result -> print result
 
 main :: IO ()
 main = start =<< getArgs
-
-    -- let (a3, b3) = runParser pNumber "-142sdf"
-    -- print $ case a3 of
-    --     Left a3' -> a3'
-    --     Right a3' -> (show a3') ++ " - " ++ b3
-
-    -- let (a3, b3) = runParser pValue "-3456"
-    -- print $ case a3 of
-    --     Left a3' -> a3'
-    --     Right a3' -> (show a3') ++ " - " ++ b3
