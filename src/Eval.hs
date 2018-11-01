993659af978a9d1e4d86a13c0628238129d77f09
Arthur Chaloin <arthur@achaloin.com>
2018-11-01 14:29:00 +0100

    seems to work with priorities

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,17 +1,21 @@
-module Eval (eval) where
+module Eval (eval, evalAst, replace) where
 
 import Syntax
 import Types
 import Data.Fixed
+import Data.Either
 
-applyFunc :: Float -> FuncAst -> Float
+replace :: [a] -> Int -> a -> [a]
+replace xs i x = (take (i - 1) xs) ++ [x] ++ (drop (i + 1) xs)
+
+applyFunc :: ValueAst -> FuncAst -> Float
 applyFunc result func = case op func of
-  '+' -> result + (evalExpr $ val func)
-  '-' -> result - (evalExpr $ val func)
-  '*' -> result * (evalExpr $ val func)
-  '/' -> result / (evalExpr $ val func)
-  '%' -> result `mod'` (evalExpr $ val func)
-  '^' -> result ** (evalExpr $ val func)
+  '+' -> (evalAst result) + (evalAst $ val func)
+  '-' -> (evalAst result) - (evalAst $ val func)
+  '*' -> (evalAst result) * (evalAst $ val func)
+  '/' -> (evalAst result) / (evalAst $ val func)
+  '%' -> (evalAst result) `mod'` (evalAst $ val func)
+  '^' -> (evalAst result) ** (evalAst $ val func)
   _ -> 0
 
 applySign :: Maybe Char -> Float -> Float
@@ -23,19 +27,35 @@ applySign sign val = case sign of
 
 processExprHead :: Maybe Char -> ValueAst -> Float
 processExprHead unaryOp (Left val) = applySign unaryOp val
-processExprHead unaryOp val@(Right expr) = applySign unaryOp $ evalExpr val
+processExprHead unaryOp val@(Right expr) = applySign unaryOp $ evalAst val
 
-processExpr :: ExprAst -> Float
-processExpr (ExprAst unaryOp header []) = processExprHead unaryOp header
-processExpr (ExprAst unaryOp header (func:funcs)) = processExpr $ ExprAst Nothing (Left (applyFunc (processExprHead unaryOp header) func)) funcs
+reduceFuncs :: Char -> ExprAst -> ExprAst
+reduceFuncs sym e@(ExprAst u (Left _) []) = e
+reduceFuncs sym e@(ExprAst u h@(Right _) []) = ExprAst u (Left $ processExprHead u h) []
+reduceFuncs sym e@(ExprAst u h (f@(FuncAst o _):fs))
+  | o == sym = reduceFuncs sym $ ExprAst u (Left $ applyFunc (Left $ processExprHead u h) f) fs
+  | otherwise = reduceFuncs' sym 1 e
+  where
+    reduceFuncs' sym i e@(ExprAst u h fs)
+      | i >= length fs = e
+      | op g == sym = reduceFuncs' sym i $ ExprAst u h $ (replace fs i (FuncAst (op f) (Left $ applyFunc (val f) g)))
+      | otherwise = reduceFuncs' sym (i + 1) e
+      where
+        fn idx = fs !! idx
+        g = fn i
+        f = fn (i - 1)
 
-evalExpr :: ValueAst -> Float
-evalExpr (Left val) = val
-evalExpr (Right expr) = processExpr expr
+evalAst :: ValueAst -> Float
+evalAst (Left val) = val
+evalAst (Right expr) = case header res of
+  Left val -> val
+  Right exp -> evalAst $ header exp
+  where
+    res = reduceFuncs '+' $ reduceFuncs '-' $ reduceFuncs '*' $ reduceFuncs '/' $ reduceFuncs '^' expr
 
 eval :: String -> Either String Float
 eval input = do
   let expr = runParser pEntry input
   case fst expr of
-    Right exp -> Right $ evalExpr exp
+    Right exp -> Right $ evalAst exp
     Left err -> Left err
