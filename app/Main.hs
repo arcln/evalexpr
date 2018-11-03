a4aeaf13ea82a14d839e275e6752a28561827e0a
Paul Laffitte <paul.laffitte@epitech.eu>
2018-11-03 23:05:21 +0100

    error on stderr + fix show result

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,25 @@ import Eval
 import System.Environment
 import Text.Printf
 import System.Exit
+import System.IO
 
 start :: [String] -> IO ()
-start [] = putStrLn "usage: ./funEvalExpr '-(2+2)*3'" >>= (\_ -> exitWith $ ExitFailure 84)
+start [] = hPutStrLn stderr "usage: ./funEvalExpr '-(2+2)*3'" >>= (\_ -> exitWith $ ExitFailure 84)
 start (x:_) = do
   case eval x of
-    Left err -> putStrLn err >>= (\_ -> exitWith $ ExitFailure 84)
+    Left err -> hPutStrLn stderr err >>= (\_ -> exitWith $ ExitFailure 84)
     Right r -> if isInfinite r
       then printResult r >>= (\_ -> exitWith $ ExitFailure 84)
       else printResult r
-    where printResult r = putStrLn $ showResult r
+    where printResult r = hPutStrLn stderr $ showResult r
 
-showResult :: (PrintfArg a, Floating a) => a -> String
-showResult = printf "%0.*f" (2 :: Int)
+showResult :: Float -> String
+showResult r
+  | r == 0 = "0"
+  | otherwise = let (_, x) = properFraction r in case x of
+      0 -> printf "%0.0f" r
+      1 -> printf "%0.1f" r
+      _ -> printf "%0.2f" r
 
 main :: IO ()
 main = start =<< getArgs
